6f945ac92f861536b21d7e26bfa86c1a80a35700
thomasmarano <thomas.marano2@gmail.com>
2019-02-06 11:51:54 +0000

    Test Commit

---
+++ jasmine/spec/AirportSpec.js
@@ -0,0 +1,17 @@
+describe("Airport", function(){
+
+    var airport;
+
+
+    beforeEach(function() {
+      airport = new Airport();
+    });
+
+    describe('should instruct a plan to land', function(){
+        it ('returns a plane after landing it', function(){
+
+
+
+        });
+    });
+});
